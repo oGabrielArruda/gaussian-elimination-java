9d007d0c02205b76b324f51dc2c3848f9e735bc1
Gabriel Arruda <gabrielalvesdearruda@gmail.com>
2019-10-28 00:30:56 -0300

    changes leitor

--- Leitor.java
+++ Leitor.java
@@ -12,32 +12,25 @@ public class Leitor
 		                           arq));
 	}
 
-	 public Matriz[] getSistemas() // retorna uma matriz de sistemas
+	 public Matriz[] getSistemas() throws Exception // retorna uma matriz de sistemas
      {
 		Matriz ret[] = null;
 		int i = 0;
-
-		try
+		while(arquivo.readLine() != null)
 		{
-			while(arquivo.readLine() != null)
+			int qtdEquacoes = Integer.parseInt(arquivo.readLine());
+
+			Matriz matriz = new Matriz(qtdEquacoes,qtdEquacoes+1);
+
+			for(int linha = 0; linha < qtdEquacoes; linha++)
 			{
-				int qtdEquacoes = Integer.parseInt(arquivo.readLine());
-
-				Matriz matriz = new Matriz(qtdEquacoes,qtdEquacoes+1);
-
-				for(int linha = 0; linha < qtdEquacoes; linha++)
-				{
-					StringTokenizer quebrador = new StringTokenizer (arquivo.readLine());
-					for(int coluna = 0; coluna < qtdEquacoes+1; coluna++)
-						matriz.setValor(linha, coluna, Double.parseDouble(quebrador.nextToken()));
-				}
-				ret[i] = matriz;
-				i++;
+				StringTokenizer quebrador = new StringTokenizer (arquivo.readLine());
+				for(int coluna = 0; coluna < qtdEquacoes+1; coluna++)
+					matriz.setValor(linha, coluna, Double.parseDouble(quebrador.nextToken()));
 			}
+			ret[i] = matriz;
+			i++;
 		}
-		catch(Exception ex)
-		{}
-
 		return ret;
      }
 }
